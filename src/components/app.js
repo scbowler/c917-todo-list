07b85f9a0761128298c08614da3ad6f29a2565fe
Scott Bowler <scott.bowler@learningfuze.com>
2017-11-02 12:55:38 -0700

    Fetched dummy data and displayed on screen

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
+import TodoList from './todo_list';
 
 const App = () => (
-    <div>
+    <div className="container">
         <h1 className="center-align">To Do List</h1>
+        <hr/>
+        <TodoList/>
     </div>
 );
 
---
+++ src/helpers/dummy_data.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        title: 'Wash car',
+        details: 'Wash the red car'
+    },
+    {
+        title: 'Clean House',
+        details: 'Go to the casino and clean house'
+    },
+    {
+        title: 'Go to store',
+        details: 'Bread, milk, eggs, and diet coke'
+    },
+    {
+        title: 'Feed the bear',
+        details: 'He\'s getting really hungry'
+    }
+];
